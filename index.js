0b065d4ce4343e6d3c9f6b5a735bbad089a30859
Facundo Fauda <facundo.fauda2000@gmail.com>
2022-11-29 13:44:33 -0300

    Armando convertor

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-//Convertidor de monedas 
+//Conversor de monedas 
 
+let Valordolar = 314;
+let Valoreuro = 308;
+
+function convertor(cantidad,Valordolar,Valoreuro,operacion){
+    switch(operacion){
+        case "dolar":
+            return cantidad / Valordolar;
+            break;
+        case "euro":
+            return cantidad / Valoreuro;
+            break;
+        default:
+            return "Operacion no valida";
+            break;
+    }
+}
+let cantidad = parseInt(prompt("Cantidad que quiere convertir de pesos Argentinos"));
+let operacion  = prompt("Elija entre Dolar y Euro") ;
+
+let resultado = calculadora(cantidad,Valordolar,Valoreuro,operacion);
+let mensaje = "El resultado de la conversion es" +" "+ resultado;
